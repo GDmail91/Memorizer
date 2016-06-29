76f1ad4a59ac673796d88909fe73bb343a218b58
GDmail <GDmail91@gmail.com>
2016-06-29 17:09:17 +0900

    Release 1.1

--- app/src/main/java/com/memorizer/memorizer/MemoAlarmActivity.java
+++ app/src/main/java/com/memorizer/memorizer/MemoAlarmActivity.java
@@ -3,7 +3,6 @@ package com.memorizer.memorizer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
