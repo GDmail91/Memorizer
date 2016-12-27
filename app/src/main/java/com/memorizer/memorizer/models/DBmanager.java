eb3b6c1dba7e7f0e8bacc476a3cf18fcc26cc810
GDmail <GDmail91@gmail.com>
2016-12-27 19:17:19 +0900

    Add DBmanager comment

--- app/src/main/java/com/memorizer/memorizer/models/DBmanager.java
+++ app/src/main/java/com/memorizer/memorizer/models/DBmanager.java
@@ -26,6 +26,7 @@ public class DBmanager extends SQLiteOpenHelper {
     private static final String COLUMN_MEMO_MINUTE = "memoTimeMinute";
     private static final String COLUMN_MEMO_POSTED = "posted";
 
+    // MemoSchedule 테이블
     private static final String TABLE_NAME_SCHEDULE = "MemoSchedule";
     private static final String COLUMN_SCHEDULE_MEMO_ID = "memoId";
     private static final String COLUMN_SCHEDULE_ALARM_DATE = "alarmDate";
@@ -56,6 +57,7 @@ public class DBmanager extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // SQLite DB 버전 관리
         Log.d("MEMOMODEL", oldVersion + " => " +newVersion);
 
         switch (oldVersion)
@@ -64,6 +66,7 @@ public class DBmanager extends SQLiteOpenHelper {
             case 2:
             case 3:
             case 4:
+                // 기존 버전
                 db.execSQL("CREATE TABLE "+TABLE_NAME_MEMO+" ( " +
                         "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         COLUMN_MEMO_CONTENT+" TEXT, " +
@@ -84,6 +87,12 @@ public class DBmanager extends SQLiteOpenHelper {
         }
     }
 
+    /**
+     * DB 재 생성 필요시
+     * @param db
+     * @param oldVersion
+     * @param newVersion
+     */
     public void onRecreate(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.d("MEMOMODEL", oldVersion + " => " +newVersion);
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME_MEMO);
