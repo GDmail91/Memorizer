e662da562cb7fd4698b4134a5e55b86aeb3791f3
GDmail <GDmail91@gmail.com>
2017-01-13 13:31:28 +0900

    Change Main memo item ui / Add label header in alarm activity

--- app/src/main/java/com/memorizer/memorizer/MemoAlarmActivity.java
+++ app/src/main/java/com/memorizer/memorizer/MemoAlarmActivity.java
@@ -3,11 +3,14 @@ package com.memorizer.memorizer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.memorizer.memorizer.models.Constants;
 import com.memorizer.memorizer.models.MemoData;
 
 import java.io.Serializable;
@@ -36,9 +39,33 @@ public class MemoAlarmActivity extends Activity implements View.OnClickListener 
         intentBundle.clear();
 
         TextView messageView = (TextView) findViewById(R.id.message_view);
+        RelativeLayout labelHeader = (RelativeLayout) findViewById(R.id.label_header);
+        TextView label = (TextView) findViewById(R.id.label);
 
         messageView.setText(memoData.getContent());
-
+        if (memoData.getLabel().equals("") && memoData.getLabelPos() == 0) {
+            labelHeader.setVisibility(View.GONE);
+        } else {
+            int color = 0;
+            switch (memoData.getLabelPos()) {
+                case Constants.COLOR_BLUE:
+                    color = R.drawable.color_selector_blue;
+                    break;
+                case Constants.COLOR_RED:
+                    color = R.drawable.color_selector_red;
+                    break;
+                case Constants.COLOR_ORANGE:
+                    color = R.drawable.color_selector_orange;
+                    break;
+                case Constants.COLOR_GREEN:
+                    color = R.drawable.color_selector_green;
+                    break;
+                default:
+                    color = R.drawable.color_selector;
+            }
+            label.setBackground(ContextCompat.getDrawable(this, color));
+            label.setText(memoData.getLabel());
+        }
     }
 
     public void onClick(View v) {
